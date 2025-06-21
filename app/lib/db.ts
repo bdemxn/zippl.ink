fdf278fd94e3776a2f762741f573745a68262642
Kevin Bonilla <kb1909zzz@gmail.com>
2025-06-21 15:51:28 -0600

    add(auth): auth-schema & db schemas are configured

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { env } from "./env";
+import * as schema from "~/schemas";
 
 const client = neon(env.DATABASE_URL);
-export const db = drizzle({ client });
+export const db = drizzle({ client, schema });
